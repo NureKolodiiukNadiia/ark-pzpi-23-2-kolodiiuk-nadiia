a04bb214f7383c6094341d82df4bdeb578c7caa6
Nadia Kolodiiuk <nkolodeyuk@gmail.com>
2025-10-25 03:11:57 +0300

    remove automapper and disable nullability

--- SpotRent.Api/ContainerConfigExtensions.cs
+++ SpotRent.Api/ContainerConfigExtensions.cs
@@ -2,9 +2,4 @@ namespace SpotRent.Api;
 
 public static class ContainerConfigExtensions
 {
-    public static void RegisterAutomapper(this IServiceCollection services)
-    {
-        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-        services.AddAutoMapper(typeof(MappingProfile));
-    }
 }
