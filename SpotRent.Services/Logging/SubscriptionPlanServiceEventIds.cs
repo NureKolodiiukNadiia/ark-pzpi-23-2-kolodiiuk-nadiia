30350f74ad5a100764ad33a8fa1f9c3672d85d53
Nadia Kolodiiuk <nkolodeyuk@gmail.com>
2025-11-22 00:45:15 +0200

    feat(logging): rewrite logging methods into generic, add event ids for subscription related features

---
+++ SpotRent.Services/Logging/SubscriptionPlanServiceEventIds.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.Logging;
+using SpotRent.Services.Subscriptions;
+
+namespace SpotRent.Services.Logging;
+
+internal static class SubscriptionPlanServiceEventIds
+{
+    internal static readonly EventId GetPlans = new(3101, nameof(SubscriptionPlanService.GetPlansAsync));
+
+    internal static readonly EventId GetPlanById = new(3102, nameof(SubscriptionPlanService.GetPlanByIdAsync));
+
+    internal static readonly EventId CreateSubscriptionPlan = new(3103, nameof(SubscriptionPlanService.CreateSubscriptionPlanAsync));
+
+    internal static readonly EventId UpdateSubscriptionPlan = new(3104, nameof(SubscriptionPlanService.UpdateSubscriptionPlanAsync));
+
+    internal static readonly EventId DeactivateSubscriptionPlan = new(3105, nameof(SubscriptionPlanService.DeactivateSubscriptionPlanAsync));
+
+    internal static readonly EventId DeleteSubscriptionPlan = new(3106, nameof(SubscriptionPlanService.DeleteSubscriptionPlanAsync));
+}
