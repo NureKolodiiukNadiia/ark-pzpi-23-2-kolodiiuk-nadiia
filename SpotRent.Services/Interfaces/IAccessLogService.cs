cee919cd5017cd5fe4a59c4d5e82caf9e728b3a2
Nadia Kolodiiuk <nkolodeyuk@gmail.com>
2025-12-09 03:48:52 +0200

    feat(acces-logs): add access logging functionality

--- SpotRent.Services/Interfaces/IAccessLogService.cs
+++ SpotRent.Services/Interfaces/IAccessLogService.cs
@@ -6,10 +6,13 @@ namespace SpotRent.Services.Interfaces;
 
 public interface IAccessLogService
 {
-    Task<Result<AccessLog>> LogAccessAsync(int userId, string deviceId, AccessType accessType, int? bookingId = null,
+    Task<Result<int>> LogAccessAsync(int userId, int deviceId, AccessType accessType, int bookingId,
         bool isSuccessful = true, string errorMessage = null);
 
-    Task<Result<IEnumerable<AccessLog>>> GetSpaceAccessLogsAsync(string deviceId);
+    Task<Result<int>> LogOwnerAccessAsync(int userId, int deviceId, AccessType accessType,
+        bool isSuccessful = true, string errorMessage = null);
+
+    Task<Result<IEnumerable<AccessLog>>> GetSpaceAccessLogsAsync(int deviceId);
 
     Task<Result<IEnumerable<AccessLog>>> GetOwnerAccessLogsAsync(int ownerId);
 
