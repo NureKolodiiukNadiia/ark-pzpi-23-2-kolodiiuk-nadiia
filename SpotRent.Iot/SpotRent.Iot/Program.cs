8c3f9565f724b1e3fcec7f124e86d15b06048b64
Nadia Kolodiiuk <nkolodeyuk@gmail.com>
2025-12-31 23:45:30 +0200

    feat: add base impl of dotnet iot

---
+++ SpotRent.Iot/SpotRent.Iot/Program.cs
@@ -0,0 +1,7 @@
+﻿using SpotRent.Iot;
+
+var options = DeviceOptions.Load("device-settings.json");
+var device = new Device(options);
+device.AddSmartLock(options.InitialLockState);
+
+await device.RunAsync();
